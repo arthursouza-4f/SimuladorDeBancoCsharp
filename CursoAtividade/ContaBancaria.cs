970aa757397bc4a5b364389a771b604e1080dbc7
arthursouza-4f <joaoanton384@gmail.com>
2020-12-16 15:05:08 -0300

    Adicionar arquivos de projeto.

---
+++ CursoAtividade/ContaBancaria.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoAtividade
+{
+    class ContaBancaria
+    {
+
+        public int NumeroDaConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+      
+
+    
+
+       
+
+        public ContaBancaria(int conta, string nome, double saldo)
+        {
+            NumeroDaConta = conta;
+            Nome = nome;
+            Saldo = saldo;
+        }
+         public ContaBancaria(int conta, string nome)
+        {
+
+            NumeroDaConta = conta;
+            Nome = nome;
+            Saldo = 0;
+
+        }
+        
+
+        public void AdicionarSaldo(double pegarsaldo)
+        {
+            Saldo += pegarsaldo; 
+        }
+
+        public void RemoverSaldo(double pegarsaldo)
+        {
+            Saldo -= pegarsaldo + 5 ;
+        }
+
+        public override string ToString()
+        {
+            return "Numero da conta: "
+                + NumeroDaConta
+                + ", Titular: "
+                + Nome
+                + ", Saldo da conta: "
+                + Saldo;
+        }
+
+
+
+
+
+    }
+}
---
+++ CursoAtividade/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CursoAtividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            ContaBancaria conta1;
+            Console.WriteLine("Programa para simular sistema de banco. ");
+            Console.Write("Numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("havera deposito inicial(s/n)? ");
+            string sim_ou_nao = Console.ReadLine();
+
+            if (sim_ou_nao == "s" || sim_ou_nao ==  "S")
+            {
+                Console.WriteLine("Digite o saldo inicial do conta ");
+                double saldo = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, saldo);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta");
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine("Entre com o valor pra deposito: ");
+            double pegarsaldo = double.Parse(Console.ReadLine());
+            conta.AdicionarSaldo(pegarsaldo);
+
+
+            Console.WriteLine("Dados atualizados");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Entre com o valor pra saque: ");
+            pegarsaldo = double.Parse(Console.ReadLine());
+            conta.RemoverSaldo(pegarsaldo);
+
+            Console.WriteLine("Dados atualizados");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
